33ba9e7fda9394b1d64d81391b3f6b6c3b99d390
Jyri Turku <jyri.turku@student.saimia.fi>
2019-09-11 11:42:18 +0300

    Full version, missing comments. (oops)

---
+++ PriceCalc/PriceCalc/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace PriceCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ika;
+            string varusmies;
+            string jasenyys;
+            string opiskelija;
+
+            Console.WriteLine("Vastatkaa näihin kysymyksiin:");
+            Console.WriteLine("Mikä on ikänne? (Pelkkä numero.)");
+            ika = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Oletteko varusmies? (k/e)");
+            varusmies = Console.ReadLine();
+
+            Console.WriteLine("Oletteko mtk:n jäsen? (k/e)");
+            jasenyys = Console.ReadLine();
+
+            Console.WriteLine("Oletteko opiskelija? (k/e)");
+            opiskelija = Console.ReadLine();
+
+            Console.WriteLine();
+            Laskenta(ika, varusmies, jasenyys, opiskelija);
+
+
+        }
+
+        static void Laskenta(int ika, string vm, string jas, string opis)
+        {
+            decimal hinta = 16;
+            decimal ale = 1;
+
+            if (ika < 7)
+            {
+                hinta = 0;
+            }
+            else if (jas == "k" && opis == "k")
+            {
+                ale = 0.40m;
+            }
+            else if (ika >= 65)
+            {
+                ale = 0.50m;
+            } else if (7 <= ika && ika <= 15)
+            {
+                ale = 0.50m;
+            } else if (vm == "k")
+            {
+                ale = 0.50m;
+            } else if (opis == "k" && jas == "e")
+            {
+                ale = 0.55m;
+            } else if (jas == "k" && opis == "e")
+            {
+                ale = 0.85m;
+            } 
+
+            hinta = Decimal.Multiply(hinta, ale);
+            Console.WriteLine("Lipun hinta on " + hinta + " euroa.");
+        }
+    }
+}
