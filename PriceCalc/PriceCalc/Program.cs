d1c03d49b999e0962d7fb5fe393da484514563b0
Jyri Turku <jyri.turku@student.saimia.fi>
2019-09-11 11:51:12 +0300

    Finished work.

--- PriceCalc/PriceCalc/Program.cs
+++ PriceCalc/PriceCalc/Program.cs
@@ -6,11 +6,13 @@ namespace PriceCalc
     {
         static void Main(string[] args)
         {
+            //Luodaan taas muuttujia.
             int ika;
             string varusmies;
             string jasenyys;
             string opiskelija;
 
+            //Konsoli kysyy arvot muuttujille yksi kerrallaan.
             Console.WriteLine("Vastatkaa näihin kysymyksiin:");
             Console.WriteLine("Mikä on ikänne? (Pelkkä numero.)");
             ika = int.Parse(Console.ReadLine());
@@ -24,17 +26,23 @@ namespace PriceCalc
             Console.WriteLine("Oletteko opiskelija? (k/e)");
             opiskelija = Console.ReadLine();
 
+            //Tulostetaan vastaus funktion kautta.
             Console.WriteLine();
             Laskenta(ika, varusmies, jasenyys, opiskelija);
 
 
         }
 
+        //Funktio laskentaa varten.
         static void Laskenta(int ika, string vm, string jas, string opis)
         {
+            //"hinta" on lipun täysihinta, "ale" on prosenttimäärä lipun maksettavasta hinnasta (100 - alennusprosentti)
+            //"ale" oletusarvona 1, jos mikään alennus ei päde niin antaa suoraan täyden hinnan.
             decimal hinta = 16;
             decimal ale = 1;
 
+            //Käydään if-rakenteella kaikki mahdolliset alennukset läpi, suurimmasta pienimpään. Suurin alennus jää täten muistiin.
+            //Vauvoille annetaan hinta suoraan nollaksi, muilla annetaan "ale" muuttujalle arvo.
             if (ika < 7)
             {
                 hinta = 0;
@@ -60,6 +68,7 @@ namespace PriceCalc
                 ale = 0.85m;
             } 
 
+            //Lasketaan ja tulostetaan lipun lopullinen hinta.
             hinta = Decimal.Multiply(hinta, ale);
             Console.WriteLine("Lipun hinta on " + hinta + " euroa.");
         }
